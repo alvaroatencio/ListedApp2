3c220d040337dc5097749085b30cb828bd0bf7f8
alvaroatencio <alvaroatenciop95@gmail.com>
2023-03-28 18:24:02 -0300

    inicializando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HomeContainerComponent } from './components/home/home-container/home-container.component';
-import { CardsContainerComponent } from './components/home/home-container/cards-container/cards-container.component';
-import { CardCuentasComponent } from './components/home/home-container/cards-container/card-cuentas/card-cuentas.component';
-import { LandingComponent } from './components/landing/landing.component';
-import { UsersContainerComponent } from './components/landing/users-container/users-container.component';
-import { LoginFormComponent } from './components/landing/users-container/login-form/login-form.component';
-import { ManageSectionComponent } from './components/landing/users-container/manage-section/manage-section.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { PanelComponent } from './components/profile/panel/panel.component';
-import { SharedComponent } from './components/shared/shared.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { NavComponent } from './components/shared/nav/nav.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HomeContainerComponent} from './components/home/home-container/home-container.component';
+import {CardsContainerComponent} from './components/home/home-container/cards-container/cards-container.component';
+import {
+  CardCuentasComponent
+} from './components/home/home-container/cards-container/card-cuentas/card-cuentas.component';
+import {LandingComponent} from './components/landing/landing.component';
+import {UsersContainerComponent} from './components/landing/users-container/users-container.component';
+import {LoginFormComponent} from './components/landing/users-container/login-form/login-form.component';
+import {ManageSectionComponent} from './components/landing/users-container/manage-section/manage-section.component';
+import {ProfileComponent} from './components/profile/profile.component';
+import {PanelComponent} from './components/profile/panel/panel.component';
+import {SharedComponent} from './components/shared/shared.component';
+import {FooterComponent} from './components/shared/footer/footer.component';
+import {NavComponent} from './components/shared/nav/nav.component';
 
-import {RouterModule, RouterOutlet} from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from "./components/home/home.component";
+import {RouterOutlet} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+import {HomeComponent} from "./components/home/home.component";
 import {FormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -34,15 +37,18 @@ import {AppComponent} from "./app.component";
     SharedComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        RouterOutlet
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterOutlet
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
